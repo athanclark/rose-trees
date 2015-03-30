1ac301428781710dd7f5d08f2034f6dfe451f8b1
Athan Clark <athan.clark@gmail.com>
2015-03-30 10:54:00 -0600

    cray

--- src/Data/Tree/Rose.hs
+++ src/Data/Tree/Rose.hs
@@ -2,11 +2,33 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Data.Tree.Rose where
 
 import Data.Tree.Rose.Internal
 
+import Data.Tree (Tree (Node))
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.Default
+import Data.Functor.Identity
 
-class RoseTree (c :: * -> *) (u :: * -> *) | c -> u where
-  (@->) :: a -> u a -> c a
+class RoseTree (c :: * -> *) (u :: * -> *) (t :: * -> *) | c -> u, c -> t where
+  (@->) :: t a -> u (c a) -> c a
+
+infixr 9 @->
+
+instance RoseTree Tree [] Identity where
+  (@->) (Identity x) xs = Node x xs
+
+-- | Tagged rose tree where tags have a Default instance, where `def` is the
+-- root node.
+data PseudoTrie t a = More (t, Maybe a) (NonEmpty (PseudoTrie t a))
+                    | Rest (NonEmpty t) a
+                    | Nil
+  deriving (Show, Eq)
+
+newtype ContainerPT t a = ContainerPT (t, Maybe a)
+
+instance Default t => RoseTree (PseudoTrie t) NonEmpty (ContainerPT t) where
+  (@->) (ContainerPT x) xs = More x xs
